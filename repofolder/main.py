be85dd8e32ea3f4c570130062dc239e67d41d499
TortoiseWolfe <JonPohlner@gmail.com>
2023-03-31 13:02:34 -0400

    altered main test and passing!

--- repofolder/main.py
+++ repofolder/main.py
@@ -1,10 +1,47 @@
-def greet(name):
-    return f"Hello, {name}!"
+def helper1_function1():
+    print("Helper 1, Function 1")
+
+
+def helper1_function2():
+    print("Helper 1, Function 2")
+
+
+def helper2_function1():
+    print("Helper 2, Function 1")
+
+
+def helper2_function2():
+    print("Helper 2, Function 2")
+
+
+def helper3_function1():
+    print("Helper 3, Function 1")
+
+
+def helper3_function2():
+    print("Helper 3, Function 2")
+
+
+def helper3_function3():
+    print("Helper 3, Function 3")
+
+
+def helper3_function4():
+    print("Helper 3, Function 4")
+
 
 def main():
-    name = input("Enter your name: ")
-    greeting = greet(name)
-    print(greeting)
+    print("Starting main function")
+    helper1_function1()
+    helper1_function2()
+    helper2_function1()
+    helper2_function2()
+    helper3_function1()
+    helper3_function2()
+    helper3_function3()
+    helper3_function4()
+    print("Finished main function")
+
 
 if __name__ == "__main__":
     main()
