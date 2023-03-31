6940a7e6b83f0d0df1b7a34692bd61a47b6d45f8
TortoiseWolfe <JonPohlner@gmail.com>
2023-03-31 12:55:22 -0400

    changed the test for next phase

--- refactored/main.py
+++ refactored/main.py
@@ -1,9 +1,47 @@
-def greet(name):
-    return f"Hello, {name}!"
+def api_helper_get_data():
+    print("API Helper: Get Data")
+
+
+def api_helper_parse_data():
+    print("API Helper: Parse Data")
+
+
+def excel_helper_read_file():
+    print("Excel Helper: Read File")
+
+
+def excel_helper_write_file():
+    print("Excel Helper: Write File")
+
+
+def database_helper_connect():
+    print("Database Helper: Connect")
+
+
+def database_helper_query():
+    print("Database Helper: Query")
+
+
+def database_helper_insert():
+    print("Database Helper: Insert")
+
+
+def database_helper_update():
+    print("Database Helper: Update")
+
 
 def main():
-    name = input("Enter your name: ")
-    print(greet(name))
+    print("Starting main function")
+    api_helper_get_data()
+    api_helper_parse_data()
+    excel_helper_read_file()
+    excel_helper_write_file()
+    database_helper_connect()
+    database_helper_query()
+    database_helper_insert()
+    database_helper_update()
+    print("Finished main function")
+
 
 if __name__ == "__main__":
     main()
--- repofolder/utils.py
+++
@@ -1,5 +0,0 @@
-def greet(name):
-    return f"Hi, {name}!"
-
-def get_name():
-    return input("Please enter your name: ")
